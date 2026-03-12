4f71927028567f5ce5582ff780bc18ae6e3b64bb
Deekshi-dd <dommithideekshitha@gmail.com>
2026-03-12 22:16:35 +0530

    Create pro3.html

---
+++ week_2/day_4/pro3.js
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Payment System</title>
+    <style>
+        body {
+            font-family: Arial;
+            text-align: center;
+            margin-top: 40px;
+        }
+        button {
+            padding: 10px;
+            margin: 10px;
+        }
+        #output {
+            margin-top: 20px;
+            font-weight: bold;
+            color: darkgreen;
+        }
+    </style>
+</head>
+<body>
+
+<h2>Payment System</h2>
+
+<button onclick="makePayment('credit')">Pay with Credit Card</button>
+<button onclick="makePayment('upi')">Pay with UPI</button>
+<button onclick="makePayment('cash')">Pay with Cash</button>
+
+<div id="output"></div>
+
+<script>
+
+class Payment {
+    pay(amount) {
+        return "Processing payment...";
+    }
+}
+
+class CreditCardPayment extends Payment {
+    pay(amount) {
+        return `Paid ₹${amount} using Credit Card`;
+    }
+}
+
+class UPIPayment extends Payment {
+    pay(amount) {
+        return `Paid ₹${amount} using UPI`;
+    }
+}
+
+
+class CashPayment extends Payment {
+    pay(amount) {
+        return `Paid ₹${amount} using Cash`;
+    }
+}
+
+function makePayment(type) {
+    let payment;
+    const amount = 500;
+
+    if (type === "credit") {
+        payment = new CreditCardPayment();
+    } else if (type === "upi") {
+        payment = new UPIPayment();
+    } else {
+        payment = new CashPayment();
+    }
+
+    document.getElementById("output").innerHTML = payment.pay(amount);
+}
+</script>
+
+</body>
+</html>
