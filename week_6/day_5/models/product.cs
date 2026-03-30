1881f27eafe430a5ecd0bfe1633eb610148b886f
Deekshi-dd <dommithideekshitha@gmail.com>
2026-03-30 22:54:20 +0530

    Create product.cs

---
+++ week_6/day_5/models/product.cs
@@ -0,0 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication3.Models
+{
+    public class Product
+    {
+        [Required(ErrorMessage = "Product ID is required")]
+        public int ProductId { get; set; }
+
+        [Required(ErrorMessage = "Product Name is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Name must be 5 to 15 characters")]
+        public string ? ProductName { get; set; }
+
+        [Required(ErrorMessage = "Price is required")]
+        public double Price { get; set; }
+
+        [Required(ErrorMessage = "Category is required")]
+        [StringLength(15, MinimumLength = 5, ErrorMessage = "Category must be 5 to 15 characters")]
+        public string ? Category { get; set; }
+    }
+}
