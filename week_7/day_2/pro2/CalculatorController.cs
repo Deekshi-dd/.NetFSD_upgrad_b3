139b5ad86bc88f226b7eee6c02857f56da51e48c
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-01 22:30:41 +0530

    Create CalculatorController.cs

---
+++ week_7/day_2/pro2/CalculatorController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    [Route("calculator")]
+    public class CalculatorController : Controller
+    {
+        // GET → Show form
+        [HttpGet("add")]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        // POST → Handle calculation
+        [HttpPost("add")]
+        public IActionResult Add(int num1, int num2)
+        {
+            int result = num1 + num2;
+
+            ViewData["Result"] = result;
+
+            return View(); // same page
+        }
+    }
+
+}
