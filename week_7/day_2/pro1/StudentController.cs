ef5363f424ebeb0faeea7912c0d2eee98bcd6883
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-01 22:28:27 +0530

    Create StudentController.cs

---
+++ week_7/day_2/pro1/StudentController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplication7.Controllers
+{
+    [Route("student")]
+    public class StudentController : Controller
+    {
+
+        // get request
+       
+        [HttpGet("register")]
+        public IActionResult Register()
+        {
+            return View();
+        }
+
+        // post - handle form
+        [HttpPost("register")]
+        public IActionResult Register(string name, int age, string course)
+        {
+            if (string.IsNullOrWhiteSpace(name) || age == 0 || string.IsNullOrWhiteSpace(course))
+            {
+                ViewBag.Message = "No Data to display";
+                return View("Display"); 
+            }
+
+            ViewBag.Name = name;
+            ViewBag.Age = age;
+            ViewBag.Course = course;
+
+            return View("Display"); // go to Display page
+        }
+
+        // display data
+        [HttpGet("display")]
+        public IActionResult Display()
+        {
+            return View();
+        }
+    }
+}
