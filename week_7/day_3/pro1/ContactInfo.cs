3a8bd0414545ccd6038e724709b905fa232ac4a8
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-01 22:43:20 +0530

    Create ContactInfo.cs

---
+++ week_7/day_3/pro1/ContactInfo.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication2.Models
+{
+    public class ContactInfo
+    {
+        [Required]
+        public int ContactId { get; set; }
+
+        [Required(ErrorMessage = "First Name is required")]
+        [StringLength(15, MinimumLength = 3)]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last Name is required")]
+        [StringLength(15, MinimumLength = 3)]
+        public string LastName { get; set; }
+
+        public string CompanyName { get; set; }
+
+        [EmailAddress]
+        public string EmailId { get; set; }
+        public long MobileNo { get; set; }
+        public string Designation { get; set; }
+    }
+}
