7071bd342dc5f6888e4c287509b78aa4cd856f4c
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-01 22:42:26 +0530

    Create ContactController.cs

---
+++ week_7/day_3/pro1/ContactController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication10.Models;
+using WebApplication10.Services;
+
+namespace WebApplication2.Controllers
+{
+    public class ContactController : Controller
+    {
+       private readonly IContactService _contactService;
+
+        // constructor injection
+       public ContactController(IContactService contactService)
+        {
+            _contactService = contactService;
+        }
+
+        public IActionResult ShowContacts()
+        {
+            var contacts = _contactService.GetAllContacts();
+            return View(contacts);
+        }
+
+        public IActionResult GetContactById(int id)
+        {
+            var contact = _contactService.GetContactById(id);
+            return View(contact);
+        }
+
+        // get request
+
+        [HttpGet]
+        public IActionResult AddContact()
+        {
+            return View();
+        }
+
+        // post request
+
+        [HttpPost]
+        public IActionResult AddContact(ContactInfo contactInfo)
+        {
+            _contactService.AddContact(contactInfo);
+            return RedirectToAction("ShowContacts");
+        }
+
+    }
+}
