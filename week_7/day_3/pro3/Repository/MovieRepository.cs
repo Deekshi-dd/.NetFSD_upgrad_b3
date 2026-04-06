b181ad51341ccbd0558605928ea0aebd1860ec8d
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:10:52 +0530

    Create MovieRepository.cs

---
+++ week_7/day_3/pro3/Repository/MovieRepository.cs
@@ -0,0 +1,48 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication11.Data;
+using WebApplication11.Models;
+
+namespace WebApplication11.Repositories
+{
+    public class MovieRepository : IMovieRepository
+    {
+        private readonly AppDbContext _context;
+
+        public MovieRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public List<Movie> GetAll()
+        {
+            return _context.Movies.ToList();
+        }
+
+        public Movie GetById(int id)
+        {
+            return _context.Movies.Find(id);
+        }
+
+        public void Add(Movie movie)
+        {
+            _context.Movies.Add(movie);
+            _context.SaveChanges();
+        }
+
+        public void Update(Movie movie)
+        {
+            _context.Movies.Update(movie);
+            _context.SaveChanges();
+        }
+
+        public void Delete(int id)
+        {
+            var movie = _context.Movies.Find(id);
+            if (movie != null)
+            {
+                _context.Movies.Remove(movie);
+                _context.SaveChanges();
+            }
+        }
+    }
+}
