a6f911138bcc38acb609c4aea0a57aebc13f2191
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:01:42 +0530

    Create program.cs

---
+++ week_7/day_3/pro2/controllers/program.cs
@@ -0,0 +1,36 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication11.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+var app = builder.Build();
+
+
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.EnsureCreated();
+}
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+app.UseRouting();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Movie}/{action=Index}/{id?}");
+
+app.Run();
