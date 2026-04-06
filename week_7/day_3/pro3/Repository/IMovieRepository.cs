1af4ab24f5ce68e3bac61dc9a3f65876844e6645
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:10:20 +0530

    Create IMovieRepository.cs

---
+++ week_7/day_3/pro3/Repository/IMovieRepository.cs
@@ -0,0 +1,13 @@
+using WebApplication11.Models;
+
+namespace WebApplication11.Repositories
+{
+    public interface IMovieRepository
+    {
+        List<Movie> GetAll();
+        Movie GetById(int id);
+        void Add(Movie movie);
+        void Update(Movie movie);
+        void Delete(int id);
+    }
+}
