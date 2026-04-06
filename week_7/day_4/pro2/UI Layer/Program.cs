7d7893b48ba05626bd572ff86ebe9007769e281e
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:52:25 +0530

    Create Program.cs

---
+++ week_7/day_4/pro2/UI Layer/Program.cs
@@ -0,0 +1,20 @@
+using DataAccessLayer.Repository;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllersWithViews();
+
+// Dependency Injection
+builder.Services.AddDbContext<AppDbContext>();
+builder.Services.AddScoped<IContactRepository, ContactRepository>();
+
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Contact}/{action=ShowContacts}/{id?}");
+
+app.Run();
