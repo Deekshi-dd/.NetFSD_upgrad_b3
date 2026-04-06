162c66ebcd4af7aba8d50e57483a9405fc28b609
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:12:03 +0530

    Create MovieService.cs

---
+++ week_7/day_3/pro3/Services/MovieService.cs
@@ -0,0 +1,40 @@
+using WebApplication11.Models;
+using WebApplication11.Repositories;
+
+namespace WebApplication11.Services
+{
+    public class MovieService : IMovieService
+    {
+        private readonly IMovieRepository _repository;
+
+        public MovieService(IMovieRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public List<Movie> GetAllMovies()
+        {
+            return _repository.GetAll();
+        }
+
+        public Movie GetMovieById(int id)
+        {
+            return _repository.GetById(id);
+        }
+
+        public void AddMovie(Movie movie)
+        {
+            _repository.Add(movie);
+        }
+
+        public void UpdateMovie(Movie movie)
+        {
+            _repository.Update(movie);
+        }
+
+        public void DeleteMovie(int id)
+        {
+            _repository.Delete(id);
+        }
+    }
+}
