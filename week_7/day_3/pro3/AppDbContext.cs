459bdfed60884849a2464b061c5824d1d5994735
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-06 22:08:58 +0530

    Create AppDbContext.cs

---
+++ week_7/day_3/pro3/AppDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using WebApplication11.Models;
+
+namespace WebApplication11.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Movie> Movies { get; set; }
+    }
+}
