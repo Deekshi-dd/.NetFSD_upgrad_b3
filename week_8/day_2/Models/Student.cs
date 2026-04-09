2247d29ba686c5f2fa0ce3992cf82ae36817694f
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-09 15:14:11 +0530

    Create Student.cs

---
+++ week_8/day_2/Models/Student.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplication14.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string StudentName { get; set; }
+
+        public int CourseId { get; set; }
+
+        // Navigation property
+        public Course Course { get; set; }
+    }
+}
