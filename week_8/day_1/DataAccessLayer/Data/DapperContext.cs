907b87172546afd8ea1c5691a74c8ebd28600b41
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-09 15:01:34 +0530

    Create DapperContext.cs

---
+++ week_8/day_1/DataAccessLayer/Data/DapperContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.Data.SqlClient;
+
+namespace DataAccessLayer.Data
+{
+    public class DapperContext
+    {
+        private readonly IConfiguration _config;
+
+        public DapperContext(IConfiguration config)
+        {
+            _config = config;
+        }
+
+        public IDbConnection CreateConnection()
+        {
+            return new SqlConnection(_config.GetConnectionString("DefaultConnection"));
+        }
+    }
+}
