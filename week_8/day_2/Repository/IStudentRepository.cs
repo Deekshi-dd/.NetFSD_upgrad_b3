809b1f1aee677782aeff7221e44d35576a38c936
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-09 15:16:00 +0530

    Create IStudentRepository.cs

---
+++ week_8/day_2/Repository/IStudentRepository.cs
@@ -0,0 +1,10 @@
+﻿using WebApplication14.Models;
+
+namespace WebApplication5.Repositories
+{
+    public interface IStudentRepository
+    {
+        IEnumerable<Student> GetStudentsWithCourse();
+        IEnumerable<Course> GetCoursesWithStudents();
+    }
+}
