9559cbd1250442224e117dba78341ba26ddd612a
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-21 22:05:29 +0530

    Create Department.cs

---
+++ week_9/day_1/pro1/Models/Department.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication17.Models
+{
+    public class Department
+    {
+        public int DepartmentId { get; set; }
+        public string DepartmentName { get; set; }
+
+        public ICollection<ContactInfo> Contacts { get; set; }
+    }
+}
