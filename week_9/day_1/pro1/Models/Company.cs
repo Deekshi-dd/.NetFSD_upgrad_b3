c753dfb8b29e6673e7a1036608f251ad48225d89
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-21 22:04:23 +0530

    Create Company.cs

---
+++ week_9/day_1/pro1/Models/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WebApplication17.Models
+{
+    public class Company
+    {
+        public int CompanyId { get; set; }
+        public string CompanyName { get; set; }
+
+        public ICollection<ContactInfo> Contacts { get; set; }
+    }
+}
