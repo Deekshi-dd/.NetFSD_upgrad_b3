d9a6a6814c8ccdbcc2d901bc5a0009e6feb3cff5
Deekshi-dd <dommithideekshitha@gmail.com>
2026-04-21 22:18:49 +0530

    Create CustomExceptions.cs

---
+++ week_9/day_1/pro2/Exceptions/CustomExceptions.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication17.Exceptions
+{
+    public class NotFoundException : Exception
+    {
+        public NotFoundException(string message) : base(message)
+        {
+        }
+    }
+}
